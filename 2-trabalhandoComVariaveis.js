5d64041b1a3fe68ca15164d6a915a736aaa64b0b
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2020-08-10 04:38:52 -0300

    Update 2-trabalhandoComVariaveis.js

--- 2-trabalhandoComVariaveis.js
+++ 2-trabalhandoComVariaveis.js
@@ -1,1 +1,10 @@
 console.log("2 - Trabalhando com Variaveis");
+
+// JavaScript é case sensitive
+
+var idade = 38;
+const nome = "Johnny";
+let ano = 2020;
+
+console.log("Meu nome é " + nome + " e tenho " + idade + " anos neste ano de " + ano + ". ");
+
