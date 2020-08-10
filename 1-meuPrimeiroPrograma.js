b70e9a85a4be63d6943af7c1a8f4eacea4acf2ef
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2020-08-10 04:18:35 -0300

    Create 1-meuPrimeiroPrograma.js

---
+++ 1-meuPrimeiroPrograma.js
@@ -0,0 +1,13 @@
+console.log("Meu Primeiro Programa: Trabalhando com variaveis.");
+
+const idade = 38;
+
+console.log("idade");
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade*2);
+console.log(idade/2);
+
+const idadeSomada = idade + 2;
+console.log(idadeSomada);
