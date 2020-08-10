f238d0423ad5c42ddcc222d32a75771c53495a6b
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2020-08-10 14:06:25 -0300

    Update 7-condicionais.js

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -6,13 +6,20 @@ const listaDeDestinos = new Array(
     `Rio de Janeiro`
 );
 
-const idadeComprador = 19;
+const idadeComprador = 15;
+const estaAcompanhado = true;
 
 if (idadeComprador >= 18) {
     console.log("Comprador maior de idade.");
     listaDeDestinos.splice(1,1);
 } else {
-    console.log("Não é maior de idade e não posso vender.");
+    // A pessoa é menor de idade.
+    if(estaAcompanhado){
+        listaDeDestinos.splice(1,1);
+        console.log("Não é maior de idade, mas esta acompanhada.");
+    } else {
+        console.log("Não é maior de idade e não posso vender.");
+    }
 }
 
 console.log("Destinos possiveis:");
