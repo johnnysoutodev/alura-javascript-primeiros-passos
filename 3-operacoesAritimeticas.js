c31d3f73547c5d7e605ff7affcf7f2fcd07be574
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2020-08-10 04:38:54 -0300

    Create 3-operacoesAritimeticas.js

---
+++ 3-operacoesAritimeticas.js
@@ -0,0 +1,14 @@
+console.log("3 - Operações Aritméticas");
+
+console.log(2+2);
+console.log(10 + 8 * 2);
+console.log((10 + 8) * 2);
+
+const ano = 2020;
+const anoDeNascimento = 1982;
+
+console.log("ano" + 2020);
+
+console.log(ano - anoDeNascimento);
+
+console.log("2" + "2");
