c6b3f9728b209b343de5bdc3640d2e127ad4751b
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2020-08-10 04:54:55 -0300

    Update 4-conversaoDeTipos.js

--- 4-conversaoDeTipos.js
+++ 4-conversaoDeTipos.js
@@ -1,5 +1,8 @@
 console.log("3 - Conversão de tipos");
 
 console.log("ano" + 2020);
-console.log("2" + "2");
+console.log("2" + "2"); // concatenou os textos
 console.log(parseInt("2") + parseInt("2"));
+console.log("10" / "2");
+console.log("7" / "2");
+console.log("Ricardo" / "2"); // Not a Number (NaN)
