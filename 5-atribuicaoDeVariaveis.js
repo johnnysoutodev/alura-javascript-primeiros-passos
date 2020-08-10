505a9899c493ea7a2a2bae4c49117b6fcf124c94
Johnny Souto <39660140+johnnysoutodev@users.noreply.github.com>
2020-08-10 05:23:36 -0300

    Update 5-atribuicaoDeVariaveis.js

--- 5-atribuicaoDeVariaveis.js
+++ 5-atribuicaoDeVariaveis.js
@@ -1,12 +1,16 @@
 console.log("5  - Atribuição de variaveis");
 
+// let <= variaveis que realmente variam.
+// const <= variaveis para nao variar.
+
 var idade = 38;
-const nome = "Johnny";
+const primeiroNome = "Johnny";
 const sobrenome = "Souto";
 let ano = 2020;
 let anoDeNascimento = 1982;
-let nomeCompleto = nome + " " + sobrenome;
-
+let nomeCompleto = primeiroNome + " " + sobrenome;
+let contador = 0;
+contador = contador + 1;
 // console.log(nome + sobrenome);
 // console.log(nome + " " + sobrenome);
 // console.log(nome, sobrenome);
